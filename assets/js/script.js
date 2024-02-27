5b6453a9e313f2e53ac51b1806ea3ab89a659688
Kirthi-Niharika <niharikakirthi8@gmail.com>
2024-02-27 21:42:50 +0530

    Modified assets/js/script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,12 @@
+let counterValue =0;
 const incrementFunction = () => {
+    counterValue++;
+    document.getElementById('counter-number').innerText = counterValue;
     console.log(`increment logged!`)
 }
 
 const decrementFunction = () => {
+    counterValue--;
+    document.getElementById('counter-number').innerText = counterValue;
     console.log(`decrement logged!`)
 }
